982267ea4e872af60761945a458139ebd6408912
Eric Maina <ericdameon8266@gmail.com>
2023-03-22 14:05:23 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute the absolute value of
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+	/* using the conditional ternary operator to write the cod*/
+{
+	return (n >= 0 ? n : -n);
+}
