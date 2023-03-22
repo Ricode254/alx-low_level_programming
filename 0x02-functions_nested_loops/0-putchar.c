8085255198a0fd6644f0588475b5810af48fe186
Eric Maina <ericdameon8266@gmail.com>
2023-03-22 06:38:38 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Print_putchar in anew line without using print(f) function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+	/**
+	* funtion main printing _putchar using _putchar function
+	*/
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+	/**
+	*This is _putchar function
+	*/
+{
+	return (write(1, &c, 1));
+}
