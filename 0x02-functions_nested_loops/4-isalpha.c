9067dd07d292d79d37277279371aa29227898681
Eric Maina <ericdameon8266@gmail.com>
2023-03-22 10:32:21 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 /**
- * __isalpha(int c) - This is a function that
+ * __isalpha - This is a function that
  * checks for alphabetic characters
  * @c: single letter input
  * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
