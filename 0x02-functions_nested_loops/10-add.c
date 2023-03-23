3e76f83e3e6541eed109569fe76821f42d3a5131
Eric Maina <ericdameon8266@gmail.com>
2023-03-22 18:42:15 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+	/*code of the function starts here*/
+{
+	/* returns the result of the sum of a and b*/
+	return (a + b);
+}
