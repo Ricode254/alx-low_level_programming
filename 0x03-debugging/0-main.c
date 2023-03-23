5d4a62a89f52c7987c10be73d942f10b71380b6e
Eric Maina <ericdameon8266@gmail.com>
2023-03-23 16:10:48 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	/*starting the body of the test*/
+{
+	int x;
+
+	/*innitializing x*/
+	x = 0;
+	/*testing*/
+	positive_or_negative(x);
+	/*return 0 if successfull*/
+	return (0);
+}
