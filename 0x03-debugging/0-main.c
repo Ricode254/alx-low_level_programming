62bb474db637b37b7332daf1d02d1723a44ad661
Eric Maina <ericdameon8266@gmail.com>
2023-03-24 02:32:52 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int x;
 
 	x = 0;
 	positive_or_negative(x);
+
 	return (0);
 }
